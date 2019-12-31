d6bba7af63675a2860b3ecffd3f2988f6ef5ebd9
Claudson Martins <claudson@outlook.com>
2019-12-30 22:45:44 -0300

    Adicionadas configurações para mac

---
+++ macos/set-defaults.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+echo 'Starting macos/set-defaults.sh'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+###############################################################################
+
+# Increase the mouse speed. Was he slow? Yes!
+defaults write -g com.apple.mouse.scaling 16
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+###############################################################################
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
