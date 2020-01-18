962f3f870d5c9ff48fa2161cfa073e52b38f2bf9
Claudson Martins <claudson@outlook.com>
2020-01-18 20:53:09 -0300

    Inicio do novo fluxo

---
+++ src/create_directories.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+. "src/utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+create_directories() {
+
+    declare -a DIRECTORIES=(
+        "$HOME/Sites"
+    )
+
+    for i in "${DIRECTORIES[@]}"; do
+        mkd "$i"
+    done
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    print_in_purple "\n • Create directories\n\n"
+    create_directories
+}
+
+main
