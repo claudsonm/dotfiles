5b45d4f3d9edc58d3b3a3a628d30d0b3bf6bdded
Claudson Martins <claudson@outlook.com>
2020-01-18 23:30:36 -0300

    Finalizados scripts de instalação linux

--- src/install/ubuntu/dev_tools.sh
+++ src/install/ubuntu/dev_tools.sh
@@ -7,4 +7,36 @@
 
 print_in_purple "\n   Development Tools\n\n"
 
-install_package "Yarn" "yarn" "--no-install-recommends"
+install_package "PHP 7.4" "php7.4"
+
+install_package "PHP 7.4 Extensions" "php7.4-{curl,mbstring,xml,mysql,sqlite3,zip}"
+
+install_package "PHP PEAR" "php-pear"
+
+install_package "PHP Dev" "php-dev"
+
+execute "sudo pecl install xdebug -y" "PHP XDebug extension"
+
+export COMPOSER_HOME=$HOME/.composer
+install_package "Composer" "composer"
+
+install_package "Node" "nodejs"
+
+install_package "Yarn" "yarn"
+
+install_package "Redis" "redis-server"
+execute "sudo printf '\n' | pecl install redis" "PHP Redis extension"
+
+install_package "MySQL" "mysql-server"
+
+install_package "SQLite" "sqlite3"
+
+install_package "Git Flow AVH" "git-flow"
+
+install_package "Gitk" "gitk"
+
+execute "composer global require laravel/installer" "Laravel Installer"
+
+execute "composer global require cpriego/valet-linux" "Valet (downloading)"
+
+execute "$HOME/.composer/vendor/bin/valet install" "Valet (installing)"
--- src/install/ubuntu/misc_tools.sh
+++ src/install/ubuntu/misc_tools.sh
@@ -8,5 +8,8 @@
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 install_package "xclip" "xclip"
+install_package "Wget" "wget"
+install_package "HTTPie" "httpie"
 install_package "Vim" "vim"
+install_package "Ack" "ack"
 install_package "Z Shell" "zsh"
