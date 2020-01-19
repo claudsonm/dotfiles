ae6247b91120f70f2f40b6f1e663abbaa92c9351
Claudson Martins <claudson@outlook.com>
2020-01-18 22:46:20 -0300

    Removida chamada desnecessaria ao zsh

--- src/install/ubuntu/misc_tools.sh
+++ src/install/ubuntu/misc_tools.sh
@@ -5,19 +5,8 @@
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-ohmyzsh_install() {
-    rm -rf $HOME/.oh-my-zsh
-    execute \
-        "curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh" \
-        "Oh My Zshell"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 install_package "xclip" "xclip"
 install_package "Vim" "vim"
 install_package "Z Shell" "zsh"
-
-ohmyzsh_install
